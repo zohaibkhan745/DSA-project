a49fd3b376f42206400918ba45a8c6c768245737
Zohaib <zohaibkhann745@gmail.com>
2024-12-04 19:01:49 +0500

    add linkedlist to add new user

--- Utility_Management.cpp
+++ Utility_Management.cpp
@@ -5,108 +5,115 @@
 #include <ctime>
 
 using namespace std;
-
-struct UtilityData {
-    string area;
-    int electricityUsage;
-    int waterUsage;
-};
-
-struct Report {
-    string area;
-    string issue;
-};
-
-vector<string> tips = {
-    "Turn off lights when not in use.",
-    "Fix water leaks to save water.",
-    "Use energy-efficient appliances.",
-    "Limit unnecessary water usage."
-};
-
-vector<UtilityData> generateUtilityData() {
-    vector<UtilityData> data;
-    vector<string> areas = {"Area A", "Area B", "Area C"};
-    srand(time(0));
-
-    for (const string& area : areas) {
-        data.push_back({area, rand() % 500 + 50, rand() % 1000 + 200});
+class Node{
+    string name;
+    int id;
+    Node(string name, int id){
+        name = this->name;
+        id = this->id;
     }
+};
+// struct UtilityData {
+//     string area;
+//     int electricityUsage;
+//     int waterUsage;
+// };
 
-    return data;
-}
+// struct Report {
+//     string area;
+//     string issue;
+// };
 
-void monitorUtilities(const vector<UtilityData>& data) {
-    for (const auto& utility : data) {
-        cout << "Area: " << utility.area 
-             << " | Electricity: " << utility.electricityUsage 
-             << " kWh | Water: " << utility.waterUsage << " liters\n";
+// vector<string> tips = {
+//     "Turn off lights when not in use.",
+//     "Fix water leaks to save water.",
+//     "Use energy-efficient appliances.",
+//     "Limit unnecessary water usage."
+// };
 
-        if (utility.electricityUsage < 100) {
-            cout << "  ALERT: Electricity outage in " << utility.area << "!\n";
-        }
-        if (utility.waterUsage > 800) {
-            cout << "  NOTICE: High water usage in " << utility.area << ".\n";
-        }
-    }
-}
+// vector<UtilityData> generateUtilityData() {
+//     vector<UtilityData> data;
+//     vector<string> areas = {"Area A", "Area B", "Area C"};
+//     srand(time(0));
 
-void reportIssue(vector<Report>& reports) {
-    string area, issue;
-    cout << "Enter the area: ";
-    cin >> ws;
-    getline(cin, area);
-    cout << "Enter the issue: ";
-    getline(cin, issue);
+//     for (const string& area : areas) {
+//         data.push_back({area, rand() % 500 + 50, rand() % 1000 + 200});
+//     }
 
-    reports.push_back({area, issue});
-    cout << "Report submitted successfully for " << area << ".\n";
-}
+//     return data;
+// }
 
-void displayTips() {
-    srand(time(0));
-    int randomIndex = rand() % tips.size();
-    cout << "Resource-Saving Tip: " << tips[randomIndex] << "\n";
-}
+// void monitorUtilities(const vector<UtilityData>& data) {
+//     for (const auto& utility : data) {
+//         cout << "Area: " << utility.area 
+//              << " | Electricity: " << utility.electricityUsage 
+//              << " kWh | Water: " << utility.waterUsage << " liters\n";
+
+//         if (utility.electricityUsage < 100) {
+//             cout << "  ALERT: Electricity outage in " << utility.area << "!\n";
+//         }
+//         if (utility.waterUsage > 800) {
+//             cout << "  NOTICE: High water usage in " << utility.area << ".\n";
+//         }
+//     }
+// }
+
+// void reportIssue(vector<Report>& reports) {
+//     string area, issue;
+//     cout << "Enter the area: ";
+//     cin >> ws;
+//     getline(cin, area);
+//     cout << "Enter the issue: ";
+//     getline(cin, issue);
+
+//     reports.push_back({area, issue});
+//     cout << "Report submitted successfully for " << area << ".\n";
+// }
+
+// void displayTips() {
+//     srand(time(0));
+//     int randomIndex = rand() % tips.size();
+//     cout << "Resource-Saving Tip: " << tips[randomIndex] << "\n";
+// }
 
 int main() {
-    vector<UtilityData> utilityData;
-    vector<Report> reports;
-    int choice;
+    // vector<UtilityData> utilityData;
+    // vector<Report> reports;
+    // int choice;
 
-    while (true) {
-        cout << "\n=== Smart City Simulation ===\n";
-        cout << "1. Generate Utility Data\n";
-        cout << "2. Monitor Utilities\n";
-        cout << "3. Report an Issue\n";
-        cout << "4. Display Resource-Saving Tip\n";
-        cout << "5. Exit\n";
-        cout << "Enter your choice: ";
-        cin >> choice;
+    // while (true) {
+    //     cout << "\n=== Smart City Simulation ===\n";
+    //     cout << "1. Generate Utility Data\n";
+    //     cout << "2. Monitor Utilities\n";
+    //     cout << "3. Report an Issue\n";
+    //     cout << "4. Display Resource-Saving Tip\n";
+    //     cout << "5. Exit\n";
+    //     cout << "Enter your choice: ";
+    //     cin >> choice;
 
-        switch (choice) {
-            case 1:
-                utilityData = generateUtilityData();
-                cout << "Utility data generated.\n";
-                break;
-            case 2:
-                if (utilityData.empty()) {
-                    cout << "No utility data available. Generate data first.\n";
-                } else {
-                    monitorUtilities(utilityData);
-                }
-                break;
-            case 3:
-                reportIssue(reports);
-                break;
-            case 4:
-                displayTips();
-                break;
-            case 5:
-                cout << "Exiting...\n";
-                return 0;
-            default:
-                cout << "Invalid choice. Please try again.\n";
-        }
-    }
+    //     switch (choice) {
+    //         case 1:
+    //             utilityData = generateUtilityData();
+    //             cout << "Utility data generated.\n";
+    //             break;
+    //         case 2:
+    //             if (utilityData.empty()) {
+    //                 cout << "No utility data available. Generate data first.\n";
+    //             } else {
+    //                 monitorUtilities(utilityData);
+    //             }
+    //             break;
+    //         case 3:
+    //             reportIssue(reports);
+    //             break;
+    //         case 4:
+    //             displayTips();
+    //             break;
+    //         case 5:
+    //             cout << "Exiting...\n";
+    //             return 0;
+    //         default:
+    //             cout << "Invalid choice. Please try again.\n";
+    //     }
+    // }
 }
---
+++ prac.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Node{
+     public:
+    string name;
+    string password;
+    string email;
+    Node* Next;
+    Node* prev;
+   
+    Node(string name, string password, string email){
+        this->name = name;
+        this->password = password;
+        this->email = email;
+        Next = NULL;
+        prev = NULL;
+    }
+};
+class userData{
+    public:
+    Node* head;
+    Node* tail;
+    userData(){
+        head = NULL;
+        tail = NULL;
+    }
+    Node* insert(){
+        string name, password, email;
+        cout<<"Name: ";
+        getline(cin, name);
+        cout<<"Password: ";
+        getline(cin, password);
+        cout<<"Email: ";
+       getline(cin, email);
+        Node* temp = new Node(name, password, email);
+        if(head == NULL){
+            head = temp;
+            tail = temp;
+        }
+        else{
+            tail->Next = temp;
+            temp->prev = tail;
+            tail = temp;
+        }
+        cout<<"Registeration Successful\n";
+        
+    }
+    void display(){
+            Node* temp = head;
+            while(temp != NULL){
+                cout<<"Name: "<<temp->name<<endl;
+                cout<<"Password: "<<temp->password<<endl;
+                cout<<"Email: "<<temp->email<<endl;
+                temp = temp->Next;
+            }
+        }
+};
+int main()
+{
+    Node* head = NULL;
+    userData userdata;
+    userdata.insert();
+    userdata.display();
+    
+   
+    return 0;
+}
