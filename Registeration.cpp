c3f942ea7d5a5174746fdda3ec1c1ca8aa3ebdb4
Zohaib <zohaibkhann745@gmail.com>
2024-12-09 18:49:33 +0500

    improve registeration functionality like the user after entering already registerd email

--- Registeration.cpp
+++ Registeration.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <limits>
 using namespace std;
 
-
-struct User {
+struct User
+{
     string email;
     string password;
     string name;
-    User* next;
+    User *next;
 };
-
-
-string hashPassword(const string &password) {
+class Registeration{
+    public:
+string hashPassword(const string &password)
+{
     string hashed = "";
-    for (char c : password) {
+    for (char c : password)
+    {
         hashed += to_string((int)c + 3); // Shift ASCII values for basic encryption
     }
     return hashed;
 }
 
-
-User* createUser(const string &email, const string &password, const string &name) {
-    User* newUser = new User();
+User *createUser(const string &email, const string &password, const string &name)
+{
+    User *newUser = new User();
     newUser->email = email;
     newUser->password = hashPassword(password); // Store hashed password
     newUser->name = name;
@@ -31,7 +34,8 @@ User* createUser(const string &email, const string &password, const string &name
 }
 
 // Function to add a new user to the linked list
-void registerUser(User*& head) {
+void registerUser(User *&head)
+{
     string name, email, password;
 
     cout << "Enter your name: ";
@@ -42,17 +46,20 @@ void registerUser(User*& head) {
     cin >> password;
 
     // Check if the email is already registered
-    User* temp = head;
-    while (temp != nullptr) {
-        if (temp->email == email) {
+    User *temp = head;
+    while (temp != nullptr)
+    {
+        if (temp->email == email)
+        {
             cout << "Email is already registered. Try logging in.\n";
+            loginUser(head);
             return;
         }
         temp = temp->next;
     }
 
     // Create a new user and add to the list
-    User* newUser = createUser(email, password, name);
+    User *newUser = createUser(email, password, name);
     newUser->next = head;
     head = newUser;
 
@@ -60,7 +67,8 @@ void registerUser(User*& head) {
 }
 
 // Function to login an existing user
-void loginUser(User* head) {
+void loginUser(User *head)
+{
     string email, password;
 
     cout << "Enter your email: ";
@@ -72,9 +80,11 @@ void loginUser(User* head) {
     string hashedPassword = hashPassword(password);
 
     // Search for the user in the linked list
-    User* temp = head;
-    while (temp != nullptr) {
-        if (temp->email == email && temp->password == hashedPassword) {
+    User *temp = head;
+    while (temp != nullptr)
+    {
+        if (temp->email == email && temp->password == hashedPassword)
+        {
             cout << "Login successful! Welcome, " << temp->name << "!\n";
             return;
         }
@@ -85,15 +95,18 @@ void loginUser(User* head) {
 }
 
 // Function to save linked list data to a file
-void saveToFile(User* head) {
+void saveToFile(User *head)
+{
     ofstream file("users.txt");
-    if (!file) {
+    if (!file)
+    {
         cout << "Error saving data to file.\n";
         return;
     }
 
-    User* temp = head;
-    while (temp != nullptr) {
+    User *temp = head;
+    while (temp != nullptr)
+    {
         file << temp->name << "," << temp->email << "," << temp->password << "\n";
         temp = temp->next;
     }
@@ -102,26 +115,32 @@ void saveToFile(User* head) {
 }
 
 // Function to load data from a file into the linked list
-void loadFromFile(User*& head) {
+void loadFromFile(User *&head)
+{
     ifstream file("users.txt");
-    if (!file) {
-        if (file.fail()) {
+    if (!file)
+    {
+        if (file.fail())
+        {
             cout << "Error opening file. Please check file permissions.\n";
-        } else {
+        }
+        else
+        {
             cout << "No previous data found.\n";
         }
         return;
     }
 
     string line, name, email, password;
-    while (getline(file, line)) {
+    while (getline(file, line))
+    {
         size_t pos1 = line.find(',');
         size_t pos2 = line.find(',', pos1 + 1);
         name = line.substr(0, pos1);
         email = line.substr(pos1 + 1, pos2 - pos1 - 1);
         password = line.substr(pos2 + 1);
 
-        User* newUser = new User();
+        User *newUser = new User();
         newUser->name = name;
         newUser->email = email;
         newUser->password = password;
@@ -133,45 +152,54 @@ void loadFromFile(User*& head) {
 }
 
 // Function to display all users (for testing/debugging)
-void displayUsers(User* head) {
+void displayUsers(User *head)
+{
     cout << "\n--- Registered Users ---\n";
-    while (head != nullptr) {
+    while (head != nullptr)
+    {
         cout << "Name: " << head->name << ", Email: " << head->email << "\n";
         head = head->next;
     }
     cout << "-------------------------\n";
 }
-
-int main() {
-    User* head = nullptr; 
-    loadFromFile(head);
+};
+int main()
+{
+    Registeration r;
+    User *head = nullptr;
+    r.loadFromFile(head);
 
     int choice;
-    while (true) {
-        cout << "\n--- User Registration and Login System ---\n";
-        cout << "1. Register\n";
-        cout << "2. Login\n";
-        cout << "3. Display All Users (Debugging Purpose)\n";
-        cout << "4. Exit\n";
-        cout << "Enter your choice: ";
-        cin >> choice;
 
-        switch (choice) {
-            case 1:
-                registerUser(head);
-                break;
-            case 2:
-                loginUser(head);
-                break;
-            case 3:
-                displayUsers(head);
-                break;
-            case 4:
-                saveToFile(head); // Save data before exiting
-                cout << "Exiting...\n";
-                return 0;
-            default:
-                cout << "Invalid choice. Try again.\n";
-        }
+    cout << "\n--- User Registration and Login System ---\n";
+    cout << "1. Register\n";
+    cout << "2. Login\n";
+    cout << "3. Display All Users (Debugging Purpose)\n";
+    cout << "4. Exit\n";
+    cout << "Enter your choice: ";
+    while (!(cin >> choice))
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<std::streamsize>::max(), '\n');
+        cout << "Invalid input. Please enter a valid number: ";
+    }
+
+    switch (choice)
+    {
+    case 1:
+        r.registerUser(head);
+        break;
+    case 2:
+        r.loginUser(head);
+        break;
+    case 3:
+        r.displayUsers(head);
+        break;
+    case 4:
+        r.saveToFile(head); // Save data before exiting
+        cout << "Exiting...\n";
+        return 0;
+    default:
+        cout << "Invalid choice. Try again.\n";
     }
 }
